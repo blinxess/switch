0418a65f8dd7ad6a01706a487da76bbf62b28fab
blinx <111649405+blinxess@users.noreply.github.com>
2023-09-22 17:24:04 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
 function switch(value: any): (cases: {[any]: () -> ()}) -> nil
-	return function(cases)
-		local case_func = cases[value]
+    return function(cases)
+	local case_func = cases[value]
 
-		if not case_func then 
-			return
-		end
-
-		case_func()
+	if not case_func then 
+	     return
 	end
+
+	case_func()
+    end
 end
